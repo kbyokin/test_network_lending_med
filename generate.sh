f1ff7c80969adcc4c27416d2fcf39a7b9ce1810b
kbyokin <kbyokin@gmail.com>
2025-03-04 13:03:16 +0900

    create channel and join with orderer

--- generate.sh
+++ generate.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
+export PATH=${PWD}/bin:$PATH
+export FABRIC_CFG_PATH=${PWD}/config
 
 # Generate crypto material
 cryptogen generate --config=./config/crypto-config.yaml
 
-# # Create channel artifacts directory
-# mkdir channel-artifacts
+# Bring up network
+docker compose -f compose/compose-test-net.yaml -f compose/docker/docker-compose-test-net.yaml up -d
 
-# # Generate genesis block
-# configtxgen -profile ChannelUsingRaft -channelID system-channel -outputBlock ./channel-artifacts/genesis.block
+# Create channel genesis block
+configtxgen -profile ChannelUsingRaft -channelID system-channel -outputBlock ./channel-artifacts/genesis.block
+
+# Channel creation
+osnadmin channel join --channelID system-channel --config-block channel-artifacts/genesis.block -o localhost:7053 --ca-file crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --client-cert crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt --client-key crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
 
 # # Generate channel configuration transaction
 # configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
