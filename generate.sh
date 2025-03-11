bce1c78b41be33260e79fd309c74a858c5b75932
kbyokin <kbyokin@gmail.com>
2025-03-11 15:47:24 +0900

    set anchor peer of hospitalA

--- generate.sh
+++ generate.sh
@@ -7,21 +7,23 @@ cryptogen generate --config=./config/crypto-config.yaml --output="organizations"
 
 # Bring up network
 docker compose -f compose/compose-test-net.yaml -f compose/docker/docker-compose-test-net.yaml up -d
+docker ps -a
 
 export FABRIC_CFG_PATH=${PWD}/configtx
+configtxgen -profile ChannelUsingRaft -channelID main-channel -outputBlock ./channel-artifacts/genesis.block
 
-export ORDERER_CA=crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
-export ORDERER_ADMIN_TLS_SIGN_CERT=crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
-export ORDERER_ADMIN_TLS_PRIVATE_KEY=crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
-
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
+export ORDERER_ADMIN_TLS_SIGN_CERT=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt
+export ORDERER_ADMIN_TLS_PRIVATE_KEY=${PWD}/organizations/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
 # Create channel genesis block
-configtxgen -profile ChannelUsingRaft -channelID system-channel -outputBlock ./channel-artifacts/genesis.block
 
 # Channel creation
-osnadmin channel join --channelID system-channel --config-block channel-artifacts/genesis.block -o localhost:7053 --ca-file "$ORDERER_CA"  --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+osnadmin channel join --channelID main-channel --config-block channel-artifacts/genesis.block -o localhost:7053 --ca-file "$ORDERER_CA"  --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
 
 # View detail channel status
-# osnadmin channel list --channelID system-channel -o localhost:7053 --ca-file crypto-config/ordererOrganizations/example.com/tlsca/tlsca.example.com-cert.pem  --client-cert crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.crt --client-key crypto-config/ordererOrganizations/example.com/orderers/orderer.example.com/tls/server.key
+osnadmin channel list --channelID main-channel -o localhost:7053 --ca-file "$ORDERER_CA"  --client-cert "$ORDERER_ADMIN_TLS_SIGN_CERT" --client-key "$ORDERER_ADMIN_TLS_PRIVATE_KEY"
+
+export FABRIC_CFG_PATH=${PWD}/config
 
 # # Add organizations to the channel
 export CORE_PEER_TLS_ENABLED=true
@@ -39,6 +41,32 @@ export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/hospitalb.
 export CORE_PEER_ADDRESS=localhost:9051
 
 peer channel join -b ./channel-artifacts/genesis.block
+
+# add delay
+sleep 5
+
+## Set anchor peer
+export CORE_PEER_LOCALMSPID=hospitalaMSP
+export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/hospitala.example.com/peers/peer0.hospitala.example.com/tls/ca.crt
+export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/hospitala.example.com/users/Admin@hospitala.example.com/msp
+export CORE_PEER_ADDRESS=localhost:7051
+
+peer channel fetch config channel-artifacts/config_block.pb -o localhost:7050 --ordererTLSHostnameOverride orderer.example.com -c main-channel --tls --cafile "$ORDERER_CA"
+
+cd channel-artifacts
+configtxlator proto_decode --input config_block.pb --type common.Block --output config_block.json
+jq '.data.data[0].payload.data.config' config_block.json > config.json
+cp config.json config_copy.json
+jq '.channel_group.groups.Application.groups.hospitalaMSP.values += {"AnchorPeers":{"mod_policy": "Admins","value":{"anchor_peers": [{"host": "peer0.hospitala.example.com","port": 7051}]},"version": "0"}}' config_copy.json > modified_config.json
+configtxlator proto_encode --input config.json --type common.Config --output config.pb
+configtxlator proto_encode --input modified_config.json --type common.Config --output modified_config.pb
+configtxlator compute_update --channel_id main-channel --original config.pb --updated modified_config.pb --output config_update.pb
+configtxlator proto_decode --input config_update.pb --type common.ConfigUpdate --output config_update.json
+echo '{"payload":{"header":{"channel_header":{"channel_id":"main-channel", "type":2}},"data":{"config_update":'$(cat config_update.json)'}}}' | jq . > config_update_in_envelope.json
+configtxlator proto_encode --input config_update_in_envelope.json --type common.Envelope --output config_update_in_envelope.pb
+
+cd ..
+peer channel update -f channel-artifacts/config_update_in_envelope.pb -c main-channel -o localhost:7050  --ordererTLSHostnameOverride orderer.example.com --tls --cafile "$ORDERER_CA"
 
 # # Generate channel configuration transaction
 # configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
