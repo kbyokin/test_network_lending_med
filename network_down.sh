f8252c8fe401631411be8a5c482e5c425b27e6be
kbyokin <kbyokin@gmail.com>
2025-06-15 12:58:16 +0900

    add remove db from script

--- network_down.sh
+++ network_down.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
+
+# Stop and remove peer and orderer containers
 docker stop peer0.hospitalb.example.com orderer.example.com peer0.hospitala.example.com
 docker rm peer0.hospitalb.example.com orderer.example.com peer0.hospitala.example.com
 
+# If rm_db argument is provided, stop and remove database containers
+if [ "$1" = "rm_db" ]; then
+    echo "Removing database containers and volumes..."
+    docker stop couchdb-peer0-hospitalb couchdb-peer0-hospitala
+    docker rm couchdb-peer0-hospitalb couchdb-peer0-hospitala
+    docker volume rm compose_couchdb-peer0-hospitalb compose_couchdb-peer0-hospitala
+fi
+
 rm -rf organizations channel-artifacts
 
-# Should delete the volumes as well, but this is a test network
-# so we don't care about the data
 docker volume prune -f
 docker volume ls
 docker volume rm compose_orderer.example.com compose_peer0.hospitala.example.com compose_peer0.hospitalb.example.com
